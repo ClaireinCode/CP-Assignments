80fa11b3a31569d574e420500e5f36094dbcd066
Claireese <claireese.underwood@gmail.com>
2023-10-13 09:31:15 -0500

    Just some set up with the csv reader and some folder creation

---
+++ classes/school.py
@@ -0,0 +1,12 @@
+# school.py
+from staff import Staff
+from student import Student
+
+
+class School:
+    def __init__(self, name):
+        self.name = name
+        self.staff = Staff.all_staff()
+        self.students = Student.all_students()
+
+
---
+++ classes/student.py
@@ -0,0 +1,25 @@
+# student.py
+import csv
+
+class Student:
+    def __init__(self, name, age, role, school_id, password):
+        self.name = name 
+        self.age = age         
+        self.password = password
+        self.role = role
+        self.school_id = school_id
+
+    @classmethod
+    def all_students(cls):
+        students = []
+        with open('./data/students.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                new_students = cls(**row)
+                students.append(new_students)
+
+        print(students)
+        return students
+
+
+Student.all_students()
